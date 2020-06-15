61bc8c8a8df29ab33b4dbd194fe833528bed903a
luizhenrique <brmrluiz@gmail.com>
2020-06-15 00:13:11 -0300

    created task apis

--- api/objects/task.php
+++ api/objects/task.php
@@ -1,4 +1,188 @@
 <?php
+class Task
+{
+    private $conn;
+    private $table_name = "task";
 
+    public $Id_Task;
+    public $Name_Task;
+    public $Description_Task;
+    public $Id_Task_Type;
+    public $Id_User;
 
-?>
+    public $Token_User;
+
+    public $Error;
+    public $Json;
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+    function createTask()
+    {
+        $verify_user_token = "SELECT Token_User FROM user WHERE Id_User =:id";
+        $vrf = $this->conn->prepare($verify_user_token);
+
+        $this->Id_User = htmlspecialchars(strip_tags($this->Id_User));
+
+        $vrf->bindParam(":id", $this->Id_User);
+        $vrf->execute();
+        if ($vrf->rowCount() != 0) {
+            $data = $vrf->fetchAll();
+            $token =  $data[0][0];
+
+            if ($token == $this->Token_User) {
+
+                $create_query = "INSERT INTO task SET Name_Task=:name, Description_Task=:description, 
+                Id_Task_Type= 1,Id_User=:user";
+                $stmt = $this->conn->prepare($create_query);
+                $this->Name_Task = htmlspecialchars(strip_tags($this->Name_Task));
+                $this->Description_Task = htmlspecialchars(strip_tags($this->Description_Task));
+                $this->Id_User = htmlspecialchars(strip_tags($this->Id_User));
+
+                $stmt->bindParam(":name", $this->Name_Task);
+                $stmt->bindParam(":description", $this->Description_Task);
+                $stmt->bindParam(":user", $this->Id_User);
+
+                if ($stmt->execute()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    function deleteTask()
+    {
+        $verify_user_token = "SELECT Token_User FROM user WHERE Id_User =:id";
+        $vrf = $this->conn->prepare($verify_user_token);
+
+        $this->Id_User = htmlspecialchars(strip_tags($this->Id_User));
+
+        $vrf->bindParam(":id", $this->Id_User);
+        $vrf->execute();
+        if ($vrf->rowCount() != 0) {
+            $data = $vrf->fetchAll();
+            $token =  $data[0][0];
+
+            if ($token == $this->Token_User) {
+                $querySelect = "SELECT * FROM  task WHERE Id_Task=:task";
+                $svrf = $this->conn->prepare($querySelect);
+                $this->Id_Task = htmlspecialchars(strip_tags($this->Id_Task));
+
+                $svrf->bindParam(":task", $this->Id_Task);
+                $svrf->execute();
+                if ($svrf->rowCount() != 0) {
+                    $queryDelete = "DELETE FROM task WHERE Id_Task=:task";
+                    $dvrf = $this->conn->prepare($queryDelete);
+
+                    $this->Id_Task = htmlspecialchars(strip_tags($this->Id_Task));
+                    $dvrf->bindParam(":task", $this->Id_Task);
+
+                    if ($dvrf->execute()) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    function listTask()
+    {
+        $verify_user_token = "SELECT Token_User FROM user WHERE Id_User =:id";
+        $vrf = $this->conn->prepare($verify_user_token);
+
+        $this->Id_User = htmlspecialchars(strip_tags($this->Id_User));
+
+        $vrf->bindParam(":id", $this->Id_User);
+        $vrf->execute();
+        if ($vrf->rowCount() != 0) {
+            $data = $vrf->fetchAll();
+            $token =  $data[0][0];
+
+            if ($token == $this->Token_User) {
+                $querySelect = "SELECT * FROM  task WHERE Id_User=:user";
+                $svrf = $this->conn->prepare($querySelect);
+                $this->Id_User = htmlspecialchars(strip_tags($this->Id_User));
+
+                $svrf->bindParam(":user", $this->Id_User);
+                $svrf->execute();
+                if ($svrf->rowCount() != 0) {
+                    $num = $svrf->rowCount();
+
+                    if ($num > 0) {
+                        $task_arr = array();
+                        $task_arr["data"] = array();
+
+                        while ($row = $svrf->fetch(PDO::FETCH_ASSOC)) {
+                            extract($row);
+
+                            $task_item = array(
+                                "Id_Task" => $Id_Task,
+                                "Name_Task" => $Name_Task,
+                                "Description_Task" => $Description_Task,
+                                "Id_Task_Type" => $Id_Task_Type,
+                                "Id_User" => $Id_User,
+
+                            );
+                            array_push($task_arr["data"], $task_item);
+                        }
+                        $this->Json = $task_arr;
+                    }
+                    return true;
+                } else {
+                    $this->Json = $arr["data"] = [];
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+    function updateTask()
+    {
+
+        $verify_user_token = "SELECT Token_User FROM user WHERE Id_User =:id";
+        $vrf = $this->conn->prepare($verify_user_token);
+
+        $this->Id_User = htmlspecialchars(strip_tags($this->Id_User));
+
+        $vrf->bindParam(":id", $this->Id_User);
+        $vrf->execute();
+        if ($vrf->rowCount() != 0) {
+            $data = $vrf->fetchAll();
+            $token =  $data[0][0];
+
+            if ($token == $this->Token_User) {
+                $querySelect = "SELECT * FROM  task WHERE Id_Task=:task";
+                $svrf = $this->conn->prepare($querySelect);
+                $this->Id_Task = htmlspecialchars(strip_tags($this->Id_Task));
+
+                $svrf->bindParam(":task", $this->Id_Task);
+                $svrf->execute();
+                if ($svrf->rowCount() != 0) {
+                    $queryUpdate = "UPDATE task SET Name_Task=:name_task, 
+                    Description_Task=:descr,Id_Task_Type =:task_type WHERE Id_Task=:task";
+                    $uvrf = $this->conn->prepare($queryUpdate);
+
+                    $this->Id_Task = htmlspecialchars(strip_tags($this->Id_Task));
+                    $this->Name_Task = htmlspecialchars(strip_tags($this->Name_Task));
+                    $this->Description_Task = htmlspecialchars(strip_tags($this->Description_Task));
+                    $this->Id_Task_Type = htmlspecialchars(strip_tags($this->Id_Task_Type));
+
+                    $uvrf->bindParam(":task", $this->Id_Task);
+                    $uvrf->bindParam(":name_task", $this->Name_Task);
+                    $uvrf->bindParam(":descr", $this->Description_Task);
+                    $uvrf->bindParam(":task_type", $this->Id_Task_Type);
+
+                    if ($uvrf->execute()) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ api/task/deleteTask.php
@@ -0,0 +1,72 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header("HTTP/1.1 200 OK");
+    die();
+}
+if ($_SERVER['REQUEST_METHOD'] == "POST" || $_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+
+
+    include_once '../config/database.php';
+    include_once '../objects/task.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $task = new Task($db);
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $header = apache_request_headers();
+    $token = $header["Token"];
+    if ($token) {
+        if (
+            !empty($data->user_id) &&
+            !empty($data->task_id)
+        ) {
+            $task->Token_User = $token;
+            $task->Id_User = $data->user_id;
+            $task->Id_Task = $data->task_id;
+            if ($task->deleteTask()) {
+
+                http_response_code(200);
+                echo json_encode(array(
+                    "message" => "Função executada com sucesso."
+                ));
+            } else {
+                http_response_code(400);
+                echo json_encode(array(
+                    "message" => "Dados incorretos.",
+                    "statusCode" => 400
+                ));
+            }
+        } else {
+            http_response_code(403);
+            echo json_encode(array(
+                "message" =>
+                "Não foi possivel obter os valores na requisição.",
+                "statusCode" => 400
+            ));
+        }
+    } else {
+        http_response_code(401);
+        echo json_encode(array(
+            "message" =>
+            "Token de Autenticação não encontrado.",
+            "statusCode" => 401
+
+        ));
+    }
+} else {
+    http_response_code(400);
+    echo json_encode(array(
+        "message" => "O tipo da requisição está incorreto.",
+        "statusCode" => 400
+    ));
+}
